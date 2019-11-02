974bc902fbda43cc9f566ab8666ae760470e2adf
Alexandre Nicolaie <alexandre.nicolaie@gmail.com>
2019-11-02 23:51:24 +0100

    refact: clean minions and master file

--- master.tf
+++ master.tf
@@ -1,39 +1,61 @@
+
+locals {
+  master_host = lookup(var.master_node.connection, "host", var.master_node.ip)
+
+  default_tls_san = [
+    var.master_node.ip,
+    lookup(var.master_node.connection, "host", var.master_node.ip)
+  ]
+  tls_san_values = concat(local.default_tls_san, var.additional_tls_san)
+  tls_san_opt    = "--tls-san ${join(" --tls-san ", local.tls_san_values)}"
+
+  install_opts = [
+    "--node-ip ${var.master_node.ip}",
+    "--cluster-domain ${var.cluster_name}",
+    "--cluster-cidr ${var.cluster_cidr}",
+    "--service-cidr ${var.cluster_service_cidr}",
+    local.tls_san_opt,
+  ]
+  install_opt = join(" ", local.install_opts)
+}
+
+
 resource "null_resource" "k3s_master" {
   triggers = {
-    uniq_sha     = sha1(join("", [var.cluster_name, var.cluster_cidr, var.cluster_service_cidr]))
-    master_ip    = "${sha1(var.master_node.ip)}"
-    master_input = "${sha1(lookup(var.master_node.connection, "host", var.master_node.ip))}"
+    master_ip    = sha1(var.master_node.ip)
+    master_input = sha1(local.master_host)
+    install_opt  = sha1(local.install_opt)
   }
 
   connection {
-    type = "${lookup(var.master_node.connection, "type", "ssh")}"
+    type = lookup(var.master_node.connection, "type", "ssh")
 
-    host     = "${lookup(var.master_node.connection, "host", var.master_node.ip)}"
-    user     = "${lookup(var.master_node.connection, "user", null)}"
-    password = "${lookup(var.master_node.connection, "password", null)}"
-    port     = "${lookup(var.master_node.connection, "port", null)}"
-    timeout  = "${lookup(var.master_node.connection, "timeout", null)}"
+    host     = lookup(var.master_node.connection, "host", var.master_node.ip)
+    user     = lookup(var.master_node.connection, "user", null)
+    password = lookup(var.master_node.connection, "password", null)
+    port     = lookup(var.master_node.connection, "port", null)
+    timeout  = lookup(var.master_node.connection, "timeout", null)
 
-    script_path    = "${lookup(var.master_node.connection, "script_path", null)}"
-    private_key    = "${lookup(var.master_node.connection, "private_key", null)}"
-    certificate    = "${lookup(var.master_node.connection, "certificate", null)}"
-    agent          = "${lookup(var.master_node.connection, "agent", null)}"
-    agent_identity = "${lookup(var.master_node.connection, "agent_identity", null)}"
-    host_key       = "${lookup(var.master_node.connection, "host_key", null)}"
+    script_path    = lookup(var.master_node.connection, "script_path", null)
+    private_key    = lookup(var.master_node.connection, "private_key", null)
+    certificate    = lookup(var.master_node.connection, "certificate", null)
+    agent          = lookup(var.master_node.connection, "agent", null)
+    agent_identity = lookup(var.master_node.connection, "agent_identity", null)
+    host_key       = lookup(var.master_node.connection, "host_key", null)
 
     # NOTE: Currently not working on Windows machines
-    # https    = "${lookup(var.master_node.connection, "https", null)}"
-    # insecure = "${lookup(var.master_node.connection, "insecure", null)}"
-    # use_ntlm = "${lookup(var.master_node.connection, "use_ntlm", null)}"
-    # cacert   = "${lookup(var.master_node.connection, "cacert", null)}"
+    # https    = lookup(var.master_node.connection, "https", null)
+    # insecure = lookup(var.master_node.connection, "insecure", null)
+    # use_ntlm = lookup(var.master_node.connection, "use_ntlm", null)
+    # cacert   = lookup(var.master_node.connection, "cacert", null)
 
-    bastion_host        = "${lookup(var.master_node.connection, "bastion_host", null)}"
-    bastion_host_key    = "${lookup(var.master_node.connection, "bastion_host_key", null)}"
-    bastion_port        = "${lookup(var.master_node.connection, "bastion_port", null)}"
-    bastion_user        = "${lookup(var.master_node.connection, "bastion_user", null)}"
-    bastion_password    = "${lookup(var.master_node.connection, "bastion_password", null)}"
-    bastion_private_key = "${lookup(var.master_node.connection, "bastion_private_key", null)}"
-    bastion_certificate = "${lookup(var.master_node.connection, "bastion_certificate", null)}"
+    bastion_host        = lookup(var.master_node.connection, "bastion_host", null)
+    bastion_host_key    = lookup(var.master_node.connection, "bastion_host_key", null)
+    bastion_port        = lookup(var.master_node.connection, "bastion_port", null)
+    bastion_user        = lookup(var.master_node.connection, "bastion_user", null)
+    bastion_password    = lookup(var.master_node.connection, "bastion_password", null)
+    bastion_private_key = lookup(var.master_node.connection, "bastion_private_key", null)
+    bastion_certificate = lookup(var.master_node.connection, "bastion_certificate", null)
   }
 
   provisioner "remote-exec" {
@@ -58,39 +80,39 @@ resource "null_resource" "k3s_master_installer" {
   depends_on = [null_resource.k3s_master]
 
   connection {
-    type = "${lookup(var.master_node.connection, "type", "ssh")}"
+    type = lookup(var.master_node.connection, "type", "ssh")
 
-    host     = "${lookup(var.master_node.connection, "host", var.master_node.ip)}"
-    user     = "${lookup(var.master_node.connection, "user", null)}"
-    password = "${lookup(var.master_node.connection, "password", null)}"
-    port     = "${lookup(var.master_node.connection, "port", null)}"
-    timeout  = "${lookup(var.master_node.connection, "timeout", null)}"
+    host     = lookup(var.master_node.connection, "host", var.master_node.ip)
+    user     = lookup(var.master_node.connection, "user", null)
+    password = lookup(var.master_node.connection, "password", null)
+    port     = lookup(var.master_node.connection, "port", null)
+    timeout  = lookup(var.master_node.connection, "timeout", null)
 
-    script_path    = "${lookup(var.master_node.connection, "script_path", null)}"
-    private_key    = "${lookup(var.master_node.connection, "private_key", null)}"
-    certificate    = "${lookup(var.master_node.connection, "certificate", null)}"
-    agent          = "${lookup(var.master_node.connection, "agent", null)}"
-    agent_identity = "${lookup(var.master_node.connection, "agent_identity", null)}"
-    host_key       = "${lookup(var.master_node.connection, "host_key", null)}"
+    script_path    = lookup(var.master_node.connection, "script_path", null)
+    private_key    = lookup(var.master_node.connection, "private_key", null)
+    certificate    = lookup(var.master_node.connection, "certificate", null)
+    agent          = lookup(var.master_node.connection, "agent", null)
+    agent_identity = lookup(var.master_node.connection, "agent_identity", null)
+    host_key       = lookup(var.master_node.connection, "host_key", null)
 
     # NOTE: Currently not working on Windows machines
-    # https    = "${lookup(var.master_node.connection, "https", null)}"
-    # insecure = "${lookup(var.master_node.connection, "insecure", null)}"
-    # use_ntlm = "${lookup(var.master_node.connection, "use_ntlm", null)}"
-    # cacert   = "${lookup(var.master_node.connection, "cacert", null)}"
+    # https    = lookup(var.master_node.connection, "https", null)
+    # insecure = lookup(var.master_node.connection, "insecure", null)
+    # use_ntlm = lookup(var.master_node.connection, "use_ntlm", null)
+    # cacert   = lookup(var.master_node.connection, "cacert", null)
 
-    bastion_host        = "${lookup(var.master_node.connection, "bastion_host", null)}"
-    bastion_host_key    = "${lookup(var.master_node.connection, "bastion_host_key", null)}"
-    bastion_port        = "${lookup(var.master_node.connection, "bastion_port", null)}"
-    bastion_user        = "${lookup(var.master_node.connection, "bastion_user", null)}"
-    bastion_password    = "${lookup(var.master_node.connection, "bastion_password", null)}"
-    bastion_private_key = "${lookup(var.master_node.connection, "bastion_private_key", null)}"
-    bastion_certificate = "${lookup(var.master_node.connection, "bastion_certificate", null)}"
+    bastion_host        = lookup(var.master_node.connection, "bastion_host", null)
+    bastion_host_key    = lookup(var.master_node.connection, "bastion_host_key", null)
+    bastion_port        = lookup(var.master_node.connection, "bastion_port", null)
+    bastion_user        = lookup(var.master_node.connection, "bastion_user", null)
+    bastion_password    = lookup(var.master_node.connection, "bastion_password", null)
+    bastion_private_key = lookup(var.master_node.connection, "bastion_private_key", null)
+    bastion_certificate = lookup(var.master_node.connection, "bastion_certificate", null)
   }
 
   provisioner "remote-exec" {
     inline = [
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${local.k3s_version} sh -s - --cluster-domain ${var.cluster_name} --cluster-cidr ${var.cluster_cidr} --service-cidr ${var.cluster_service_cidr} --tls-san ${var.master_node.ip} --tls-san ${lookup(var.master_node.connection, "host", var.master_node.ip)}",
+      "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${local.k3s_version} sh -s - ${local.install_opt}",
       "until kubectl get nodes | grep -v '[WARN] No resources found'; do sleep 1; done"
     ]
   }
--- minions.tf
+++ minions.tf
@@ -1,42 +1,52 @@
+locals {
+  minion_install_opts = [
+    "INSTALL_K3S_VERSION=${local.k3s_version}",
+    "K3S_URL=https://${var.master_node.ip}:6443",
+    "K3S_TOKEN=$(cat /etc/rancher/k3s/server/node-token)"
+  ]
+  minion_install_opt = join(" ", local.minion_install_opts)
+}
+
+
 resource "null_resource" "k3s_minions" {
-  count = "${length(var.minion_nodes)}"
+  count = length(var.minion_nodes)
 
   triggers = {
     master_init  = null_resource.k3s_master.id
-    minion_ip    = "${sha1(element(var.minion_nodes.*.ip, count.index))}"
-    minion_input = "${sha1(lookup(element(var.minion_nodes.*.connection, count.index), "host", element(var.minion_nodes.*.ip, count.index)))}"
+    minion_ip    = sha1(element(var.minion_nodes.*.ip, count.index))
+    minion_input = sha1(lookup(element(var.minion_nodes.*.connection, count.index), "host", element(var.minion_nodes.*.ip, count.index)))
   }
   depends_on = [null_resource.k3s_master_installer]
 
   connection {
-    type = "${lookup(element(var.minion_nodes.*.connection, count.index), "type", "ssh")}"
+    type = lookup(element(var.minion_nodes.*.connection, count.index), "type", "ssh")
 
-    host     = "${lookup(element(var.minion_nodes.*.connection, count.index), "host", element(var.minion_nodes.*.ip, count.index))}"
-    user     = "${lookup(element(var.minion_nodes.*.connection, count.index), "user", null)}"
-    password = "${lookup(element(var.minion_nodes.*.connection, count.index), "password", null)}"
-    port     = "${lookup(element(var.minion_nodes.*.connection, count.index), "port", null)}"
-    timeout  = "${lookup(element(var.minion_nodes.*.connection, count.index), "timeout", null)}"
+    host     = lookup(element(var.minion_nodes.*.connection, count.index), "host", element(var.minion_nodes.*.ip, count.index))
+    user     = lookup(element(var.minion_nodes.*.connection, count.index), "user", null)
+    password = lookup(element(var.minion_nodes.*.connection, count.index), "password", null)
+    port     = lookup(element(var.minion_nodes.*.connection, count.index), "port", null)
+    timeout  = lookup(element(var.minion_nodes.*.connection, count.index), "timeout", null)
 
-    script_path    = "${lookup(element(var.minion_nodes.*.connection, count.index), "script_path", null)}"
-    private_key    = "${lookup(element(var.minion_nodes.*.connection, count.index), "private_key", null)}"
-    certificate    = "${lookup(element(var.minion_nodes.*.connection, count.index), "certificate", null)}"
-    agent          = "${lookup(element(var.minion_nodes.*.connection, count.index), "agent", null)}"
-    agent_identity = "${lookup(element(var.minion_nodes.*.connection, count.index), "agent_identity", null)}"
-    host_key       = "${lookup(element(var.minion_nodes.*.connection, count.index), "host_key", null)}"
+    script_path    = lookup(element(var.minion_nodes.*.connection, count.index), "script_path", null)
+    private_key    = lookup(element(var.minion_nodes.*.connection, count.index), "private_key", null)
+    certificate    = lookup(element(var.minion_nodes.*.connection, count.index), "certificate", null)
+    agent          = lookup(element(var.minion_nodes.*.connection, count.index), "agent", null)
+    agent_identity = lookup(element(var.minion_nodes.*.connection, count.index), "agent_identity", null)
+    host_key       = lookup(element(var.minion_nodes.*.connection, count.index), "host_key", null)
 
     # NOTE: Currently not working on Windows machines
-    # https    = "${lookup(element(var.minion_nodes.*.connection, count.index), "https", null)}"
-    # insecure = "${lookup(element(var.minion_nodes.*.connection, count.index), "insecure", null)}"
-    # use_ntlm = "${lookup(element(var.minion_nodes.*.connection, count.index), "use_ntlm", null)}"
-    # cacert   = "${lookup(element(var.minion_nodes.*.connection, count.index), "cacert", null)}"
+    # https    = lookup(element(var.minion_nodes.*.connection, count.index), "https", null)
+    # insecure = lookup(element(var.minion_nodes.*.connection, count.index), "insecure", null)
+    # use_ntlm = lookup(element(var.minion_nodes.*.connection, count.index), "use_ntlm", null)
+    # cacert   = lookup(element(var.minion_nodes.*.connection, count.index), "cacert", null)
 
-    bastion_host        = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_host", null)}"
-    bastion_host_key    = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_host_key", null)}"
-    bastion_port        = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_port", null)}"
-    bastion_user        = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_user", null)}"
-    bastion_password    = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_password", null)}"
-    bastion_private_key = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_private_key", null)}"
-    bastion_certificate = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_certificate", null)}"
+    bastion_host        = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_host", null)
+    bastion_host_key    = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_host_key", null)
+    bastion_port        = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_port", null)
+    bastion_user        = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_user", null)
+    bastion_password    = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_password", null)
+    bastion_private_key = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_private_key", null)
+    bastion_certificate = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_certificate", null)
   }
 
   provisioner "remote-exec" {
@@ -47,7 +57,9 @@ resource "null_resource" "k3s_minions" {
       "echo >&2 [NOTE] This behaviour will change only when we are able to download a file from the remote.",
       "rm -rf /etc/rancher/k3s/server",
       "mkdir -p /etc/rancher/k3s/server",
-      "scp -P ${lookup(var.master_node.connection, "port", "22")} -o 'StrictHostKeyChecking no' ${lookup(var.master_node.connection, "user", "root")}@${var.master_node.ip}:/var/lib/rancher/k3s/server/node-token /etc/rancher/k3s/server"
+      "scp -P ${lookup(var.master_node.connection, "port", "22")} -o 'StrictHostKeyChecking no' ${lookup(var.master_node.connection, "user", "root")}@${var.master_node.ip}:/var/lib/rancher/k3s/server/node-token /etc/rancher/k3s/server",
+      "scp -P ${lookup(var.master_node.connection, "port", "22")} -o 'StrictHostKeyChecking no' ${lookup(var.master_node.connection, "user", "root")}@${var.master_node.ip}:/etc/rancher/k3s/k3s.yaml /etc/rancher/k3s/k3s.yaml",
+      "sed -i 's|https://127.0.0.1:6443|https://${var.master_node.ip}:6443|g' /etc/rancher/k3s/k3s.yaml",
     ]
   }
 
@@ -69,39 +81,39 @@ resource "null_resource" "k3s_minions_installer" {
   depends_on = [null_resource.k3s_minions]
 
   connection {
-    type = "${lookup(element(var.minion_nodes.*.connection, count.index), "type", "ssh")}"
+    type = lookup(element(var.minion_nodes.*.connection, count.index), "type", "ssh")
 
-    host     = "${lookup(element(var.minion_nodes.*.connection, count.index), "host", element(var.minion_nodes.*.ip, count.index))}"
-    user     = "${lookup(element(var.minion_nodes.*.connection, count.index), "user", null)}"
-    password = "${lookup(element(var.minion_nodes.*.connection, count.index), "password", null)}"
-    port     = "${lookup(element(var.minion_nodes.*.connection, count.index), "port", null)}"
-    timeout  = "${lookup(element(var.minion_nodes.*.connection, count.index), "timeout", null)}"
+    host     = lookup(element(var.minion_nodes.*.connection, count.index), "host", element(var.minion_nodes.*.ip, count.index))
+    user     = lookup(element(var.minion_nodes.*.connection, count.index), "user", null)
+    password = lookup(element(var.minion_nodes.*.connection, count.index), "password", null)
+    port     = lookup(element(var.minion_nodes.*.connection, count.index), "port", null)
+    timeout  = lookup(element(var.minion_nodes.*.connection, count.index), "timeout", null)
 
-    script_path    = "${lookup(element(var.minion_nodes.*.connection, count.index), "script_path", null)}"
-    private_key    = "${lookup(element(var.minion_nodes.*.connection, count.index), "private_key", null)}"
-    certificate    = "${lookup(element(var.minion_nodes.*.connection, count.index), "certificate", null)}"
-    agent          = "${lookup(element(var.minion_nodes.*.connection, count.index), "agent", null)}"
-    agent_identity = "${lookup(element(var.minion_nodes.*.connection, count.index), "agent_identity", null)}"
-    host_key       = "${lookup(element(var.minion_nodes.*.connection, count.index), "host_key", null)}"
+    script_path    = lookup(element(var.minion_nodes.*.connection, count.index), "script_path", null)
+    private_key    = lookup(element(var.minion_nodes.*.connection, count.index), "private_key", null)
+    certificate    = lookup(element(var.minion_nodes.*.connection, count.index), "certificate", null)
+    agent          = lookup(element(var.minion_nodes.*.connection, count.index), "agent", null)
+    agent_identity = lookup(element(var.minion_nodes.*.connection, count.index), "agent_identity", null)
+    host_key       = lookup(element(var.minion_nodes.*.connection, count.index), "host_key", null)
 
     # NOTE: Currently not working on Windows machines
-    # https    = "${lookup(element(var.minion_nodes.*.connection, count.index), "https", null)}"
-    # insecure = "${lookup(element(var.minion_nodes.*.connection, count.index), "insecure", null)}"
-    # use_ntlm = "${lookup(element(var.minion_nodes.*.connection, count.index), "use_ntlm", null)}"
-    # cacert   = "${lookup(element(var.minion_nodes.*.connection, count.index), "cacert", null)}"
+    # https    = lookup(element(var.minion_nodes.*.connection, count.index), "https", null)
+    # insecure = lookup(element(var.minion_nodes.*.connection, count.index), "insecure", null)
+    # use_ntlm = lookup(element(var.minion_nodes.*.connection, count.index), "use_ntlm", null)
+    # cacert   = lookup(element(var.minion_nodes.*.connection, count.index), "cacert", null)
 
-    bastion_host        = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_host", null)}"
-    bastion_host_key    = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_host_key", null)}"
-    bastion_port        = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_port", null)}"
-    bastion_user        = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_user", null)}"
-    bastion_password    = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_password", null)}"
-    bastion_private_key = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_private_key", null)}"
-    bastion_certificate = "${lookup(element(var.minion_nodes.*.connection, count.index), "bastion_certificate", null)}"
+    bastion_host        = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_host", null)
+    bastion_host_key    = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_host_key", null)
+    bastion_port        = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_port", null)
+    bastion_user        = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_user", null)
+    bastion_password    = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_password", null)
+    bastion_private_key = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_private_key", null)
+    bastion_certificate = lookup(element(var.minion_nodes.*.connection, count.index), "bastion_certificate", null)
   }
 
   provisioner "remote-exec" {
     inline = [
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${local.k3s_version} K3S_URL=https://${var.master_node.ip}:6443 K3S_TOKEN=$(cat /etc/rancher/k3s/server/node-token) sh -"
+      "curl -sfL https://get.k3s.io | ${local.minion_install_opt} sh -s - --node-ip ${element(var.minion_nodes.*.ip, count.index)}"
     ]
   }
 }
