982f55e367905f2fa1bb628b4864562b337232f8
Alexandre NICOLAIE <anicolaie@ippon.fr>
2019-12-04 13:37:00 +0100

    feat: upload k3s version only if latest is chosen

--- k3s_version.tf
+++ k3s_version.tf
@@ -1,7 +1,8 @@
 data "http" "k3s_version" {
+  count = var.k3s_version == "latest" ? 1 : 0
   url = "https://api.github.com/repos/rancher/k3s/releases/latest"
 }
 
 locals {
-  k3s_version = var.k3s_version == "latest" ? jsondecode(data.http.k3s_version.body).tag_name : var.k3s_version
+  k3s_version = var.k3s_version == "latest" ? jsondecode(data.http.k3s_version[0].body).tag_name : var.k3s_version
 }
