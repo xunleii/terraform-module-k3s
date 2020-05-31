9e5c43cf09f8692297cbbb63853e799bf68e71b3
Alexandre Nicolaie <alexandre.nicolaie@gmail.com>
2020-05-31 12:46:48 +0200

    fix: format main.tf file

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = "~> 0.12"
   experiments      = [variable_validation]
   required_providers {
-    http     = "~> 1.2"
-    null     = "~> 2.1"
-    random   = "~> 2.2"
+    http   = "~> 1.2"
+    null   = "~> 2.1"
+    random = "~> 2.2"
   }
 }
 
