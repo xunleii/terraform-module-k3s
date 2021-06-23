383fe5b1355e7e9bffe46b5f04944dd10cedfbed
Alexandre Nicolaie dit Clairville <alexandre.nicolaie@gmail.com>
2021-06-23 22:38:28 +0200

    refactor(inputs): use `cluster_domain` instead of `name`
    The use of `cluster_domain` is clearer than just` name`, which could be interpreted as something
    else.
    
    BREAKING CHANGE: Deprecation of `name` variable
    
    fix #53

--- server_nodes.tf
+++ server_nodes.tf
@@ -81,7 +81,7 @@ locals {
         [
           "--node-ip ${server.ip}",
           "--node-name '${try(server.name, key)}'",
-          "--cluster-domain '${var.name}'",
+          "--cluster-domain '${var.cluster_domain}'",
           "--cluster-cidr ${var.cidr.pods}",
           "--service-cidr ${var.cidr.services}",
           "--token ${random_password.k3s_cluster_secret.result}",
@@ -100,7 +100,7 @@ locals {
       )))
 
       immutable_fields_hash = sha1(join("", concat(
-        [var.name, var.cidr.pods, var.cidr.services],
+        [var.cluster_domain, var.cidr.pods, var.cidr.services],
         var.global_flags,
         try(server.flags, []),
       )))
