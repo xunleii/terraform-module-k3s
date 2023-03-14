fe16e87850c34dbdb75795009c9697c41daf7c9b
Alexandre NICOLAIE <xunleii@users.noreply.github.com>
2023-03-14 19:20:07 +0100

    Update hetzner example
    - Remove Kubernetes service account creation
    - Manage private SSH key internally

--- examples/hcloud-k3s/k3s.tf
+++ examples/hcloud-k3s/k3s.tf
@@ -21,7 +21,8 @@ module "k3s" {
     hcloud_server.control_planes[i].name => {
       ip = hcloud_server_network.control_planes[i].ip
       connection = {
-        host = hcloud_server.control_planes[i].ipv4_address
+        host        = hcloud_server.control_planes[i].ipv4_address
+        private_key = trimspace(tls_private_key.ed25519-provisioning.private_key_pem)
       }
       flags       = ["--disable-cloud-controller"]
       annotations = { "server_id" : i } // theses annotations will not be managed by this module
@@ -34,54 +35,12 @@ module "k3s" {
       name = hcloud_server.agents[i].name
       ip   = hcloud_server_network.agents_network[i].ip
       connection = {
-        host = hcloud_server.agents[i].ipv4_address
+        host        = hcloud_server.agents[i].ipv4_address
+        private_key = trimspace(tls_private_key.ed25519-provisioning.private_key_pem)
       }
 
       labels = { "node.kubernetes.io/pool" = hcloud_server.agents[i].labels.nodepool }
       taints = { "dedicated" : hcloud_server.agents[i].labels.nodepool == "gpu" ? "gpu:NoSchedule" : null }
     }
-  }
-}
-
-provider "kubernetes" {
-  host                   = module.k3s.kubernetes.api_endpoint
-  cluster_ca_certificate = module.k3s.kubernetes.cluster_ca_certificate
-  client_certificate     = module.k3s.kubernetes.client_certificate
-  client_key             = module.k3s.kubernetes.client_key
-}
-
-resource "kubernetes_service_account" "bootstrap" {
-  depends_on = [module.k3s.kubernetes_ready]
-
-  metadata {
-    name      = "bootstrap"
-    namespace = "default"
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "boostrap" {
-  depends_on = [module.k3s.kubernetes_ready]
-
-  metadata {
-    name = "bootstrap"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = "bootstrap"
-    namespace = "default"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "admin"
-  }
-}
-
-data "kubernetes_secret" "sa_credentials" {
-  metadata {
-    name      = kubernetes_service_account.bootstrap.default_secret_name
-    namespace = "default"
   }
 }
