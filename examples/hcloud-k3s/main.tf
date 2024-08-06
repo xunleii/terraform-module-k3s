bb82a01c3555d140c6a07e789c3f9f8efc0bb2a5
Simon Emms <simon@simonemms.com>
2024-08-06 19:36:10 +0000

    chore(hcloud): create firewall and load balancer for hcloud
    Signed-off-by: Simon Emms <simon@simonemms.com>

--- examples/hcloud-k3s/main.tf
+++ examples/hcloud-k3s/main.tf
@@ -23,18 +23,116 @@ resource "hcloud_network_subnet" "k3s_nodes" {
   ip_range     = "10.254.1.0/24"
 }
 
+resource "hcloud_firewall" "firewall" {
+  name = "firewall"
+
+  dynamic "rule" {
+    for_each = [for each in [
+      {
+        description = "SSH port"
+        port        = 22
+        source_ips = [
+          "0.0.0.0/0"
+        ]
+      },
+      {
+        description = "Allow all TCP traffic on private network"
+        source_ips = [
+          hcloud_network.k3s.ip_range
+        ]
+      },
+      {
+        description = "Allow all UDP traffic on private network"
+        source_ips = [
+          hcloud_network.k3s.ip_range
+        ]
+        protocol = "udp"
+      },
+      # Direct public access only allowed if single manager node
+      {
+        description = "Allow access to Kubernetes API"
+        port        = "6443"
+        source_ips = [
+          "0.0.0.0/0"
+        ]
+        disabled = var.servers_num > 1
+      }
+    ] : each if lookup(each, "disabled", false) != true]
+
+    content {
+      description     = lookup(rule.value, "description", "")
+      destination_ips = lookup(rule.value, "destination_ips", [])
+      direction       = lookup(rule.value, "direction", "in")
+      port            = lookup(rule.value, "port", "any")
+      protocol        = lookup(rule.value, "protocol", "tcp")
+      source_ips      = lookup(rule.value, "source_ips", [])
+    }
+  }
+
+  apply_to {
+    label_selector = "provisioner=terraform"
+  }
+}
+
+resource "hcloud_load_balancer" "control_plane" {
+  count = var.servers_num > 1 ? 1 : 0
+
+  name               = "load_balancer"
+  load_balancer_type = "lb11"
+  network_zone       = "eu-central"
+
+  algorithm {
+    type = "round_robin"
+  }
+
+  labels = {
+    provisioner = "terraform",
+  }
+}
+
+resource "hcloud_load_balancer_network" "control_plane" {
+  count = var.servers_num > 1 ? 1 : 0
+
+  load_balancer_id = hcloud_load_balancer.control_plane[count.index].id
+  network_id       = hcloud_network.k3s.id
+
+  depends_on = [
+    hcloud_network_subnet.k3s_nodes
+  ]
+}
+#
+resource "hcloud_load_balancer_service" "kube_api" {
+  count = var.servers_num > 1 ? 1 : 0
+
+  load_balancer_id = hcloud_load_balancer.control_plane[count.index].id
+  protocol         = "tcp"
+  listen_port      = "6443"
+  destination_port = "6443"
+}
+
+resource "hcloud_load_balancer_target" "control_plane" {
+  count = var.servers_num > 1 ? 1 : 0
+
+  load_balancer_id = hcloud_load_balancer.control_plane[count.index].id
+  type             = "label_selector"
+  label_selector   = join(",", [for key, value in hcloud_server.control_planes[0].labels : "${key}=${value}"])
+  use_private_ip   = true
+
+  depends_on = [
+    hcloud_load_balancer_network.control_plane
+  ]
+}
+
 resource "hcloud_server_network" "control_planes" {
   count     = var.servers_num
   subnet_id = hcloud_network_subnet.k3s_nodes.id
   server_id = hcloud_server.control_planes[count.index].id
-  ip        = cidrhost(hcloud_network_subnet.k3s_nodes.ip_range, 1 + count.index)
 }
 
 resource "hcloud_server_network" "agents_network" {
   count     = length(hcloud_server.agents)
   server_id = hcloud_server.agents[count.index].id
   subnet_id = hcloud_network_subnet.k3s_nodes.id
-  ip        = cidrhost(hcloud_network_subnet.k3s_nodes.ip_range, 1 + var.servers_num + count.index)
 }
 
 resource "hcloud_server" "control_planes" {
